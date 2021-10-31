7f7112a79fdfcd83641806b72dbbe2fc942a1c6b
Ram-github-account <ramesh.ram569@gmail.com>
2021-10-31 09:48:08 +0530

    SpringBootCrudAppn project is committed - first commit

---
+++ src/main/java/com/spring/boot/crud/application/SpringBootCrudAppnApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.crud.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudAppnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudAppnApplication.class, args);
+	}
+
+}
